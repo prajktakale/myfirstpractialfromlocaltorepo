939a44e270dd7bbe6d13ef0f462298be26542bbf
Prajkta <prajktakale0512@gmail.com>
2025-08-19 16:16:59 +0530

    added the hello word file for jenkins demo

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+// This is a simple Java program to print "Hello, World!"
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
