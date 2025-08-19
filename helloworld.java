fe2cba100b1bd8827dac2f7224baad3618b7cabe
Prajkta <prajktakale0512@gmail.com>
2025-08-19 16:22:20 +0530

    change the class name in java program

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 // This is a simple Java program to print "Hello, World!"
-public class helloWorld {
+public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
