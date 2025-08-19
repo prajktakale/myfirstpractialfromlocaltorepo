5240dc368c651592ad1ab648a2ab9508ff181af2
Prajkta <prajktakale0512@gmail.com>
2025-08-19 16:25:21 +0530

    added the new line for automated build on jenkins

--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,5 @@
 // This is a simple Java program to print "Hello, World!"
+//THis is a new line added just to check build is get trigered automatically or not
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
